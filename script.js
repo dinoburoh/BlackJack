615fd5c5cc06f741a31dd79912e600729b3e9ef6
dinoburoh <dinoburoh@gmail.com>
2024-01-12 00:47:08 +0530

    Enhaced the functionality using reset button

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-let cardText = document.getElementById("cards")
-let sumText = document.getElementById("sum")
-let startGameText = document.getElementById("start-game")
-let newCardButton = document.querySelector("#newcardbutton")
-let startButton = document.querySelector("#startbutton")
+const cardText = document.getElementById("cards")
+const sumText = document.getElementById("sum")
+const startGameText = document.getElementById("start-game")
+const newCardButton = document.querySelector("#newcardbutton")
+const startButton = document.querySelector("#startbutton")
+const resetButton = document.querySelector("#resetbutton")
 let playerDetail = document.getElementById("player-detail")
 let sum = 0, i = 0
 let isAlive = false
 let hasBlackJack = false
 let cards = []
 
+newCardButton.disabled = true
+
 let player = {
     name: "Per",
     chips: 45
 }
 
+resetButton.addEventListener("click", function () {
+    i = 0, sum = 0
+    hasBlackJack = false, isAlive = false
+    cards = []
+    sumText.textContent = "Sum: " 
+    cardText.textContent = "Cards: " 
+    startGameText.textContent = "Want to play a round ?"
+    startButton.disabled = false
+    newCardButton.disabled = true
+    // renderGame()
+})
+
 playerDetail.textContent = player.name + ": $ " + player.chips
 
 function getRandomCard() {
     let randomCard = Math.floor(Math.random() * 13) + 1
-    if(randomCard > 10)
+    if (randomCard > 10)
         return 10
-    else if(randomCard === 1)
-        return 11
+    else if (randomCard === 1) {
+        if (sum == 20)
+            return 1
+        else
+            return 11
+    }
     else
-        return randomCard 
+    return randomCard
 }
 
 function newCard() {
@@ -35,7 +54,7 @@ function newCard() {
 function startGame() {
     isAlive = true
     cards.push(getRandomCard())
-    cards.push(getRandomCard())  
+    cards.push(getRandomCard())
     renderGame()
 }
 
@@ -52,13 +71,13 @@ function renderGame() {
     if (sum < 21 && isAlive) {
         startGameText.textContent = "Do you want to draw a new card? "
         startButton.disabled = true
+        newCardButton.disabled = false
     }
 
     else if (sum == 21) {
         startGameText.textContent = "YOU WON"
-        isAlive = false
         hasBlackJack = true
-        newCardButton.disabled = "true"
+        newCardButton.disabled = true
     }
 
     else {
